64400b349f8bad8ac6582af4d381c0f64128f860
Cocoa <i@uwucocoa.moe>
2024-01-30 11:17:20 +0700

    ci build scripts

--- .github/scripts/stage2.sh
+++ .github/scripts/stage2.sh
@@ -13,7 +13,7 @@ apt-get install -y curl gcc g++ git build-essential gettext \
     libnl-3-dev libudev-dev flex libnfs-dev libssh-dev libssh2-1-dev libpng-dev \
     bison libusb-dev libsnappy-dev libsdl2-dev libpam0g-dev libbz2-dev liblzma-dev \
     libzstd-dev libcap-ng-dev libjpeg-dev libvde-dev libvdeplug-dev liblzo2-dev \
-    libspice-server-dev libspice-protocol-dev python3-full python3-pip python3-setuptools
+    libspice-server-dev libspice-protocol-dev python3 python3-pip python3-setuptools
 
 mkdir -p /work/build
 chmod a+rw /work/build
